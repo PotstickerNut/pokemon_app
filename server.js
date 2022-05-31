b7ccc42d8af1765cf1eddeac66a4e7511adc5518
Janette Campbell <campbjp@hotmail.com>
2022-05-31 09:51:38 -0400

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const app = express();
 
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
 });
 
 app.get("/pokemon", (req, res) => {
-  res.send(pokemon);
+  res.render("Index");
 });
 
 app.listen(PORT, () => {
